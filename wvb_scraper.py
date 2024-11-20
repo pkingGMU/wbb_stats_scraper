58ddb84d91ce0a72e34998cc725a8ca61a5a4872
pkingGMU <pking23@gmu.edu>
2024-11-20 16:47:22 -0500

    wvb - create firefox webdriver

--- wvb_scraper.py
+++ wvb_scraper.py
@@ -6,4 +6,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
+# Create driver from selenium
+driver = webdriver.Firefox()
+
+# Get stats page of Womens Volleyball
+url = 'https://gomason.com/sports/womens-volleyball/stats'
+driver.get(url)
+
+time.sleep(3)
+
 
