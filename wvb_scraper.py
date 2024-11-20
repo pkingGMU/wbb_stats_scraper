866cf1575db974d086b0e57495a96ceb23b78859
pkingGMU <pking23@gmu.edu>
2024-11-20 16:46:56 -0500

    import selenium and requests

---
+++ wvb_scraper.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+
