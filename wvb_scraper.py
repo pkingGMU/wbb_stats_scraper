83f24dc8efb9e66b4aea24fa6685c625684bedf0
pkingGMU <pking23@gmu.edu>
2024-11-20 16:47:43 -0500

    wvb - Remove consent window

--- wvb_scraper.py
+++ wvb_scraper.py
@@ -14,5 +14,26 @@ url = 'https://gomason.com/sports/womens-volleyball/stats'
 driver.get(url)
 
 time.sleep(3)
+### Consent Button ###
+
+#WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[contains(@class, 'privacy-policy-notice-with-close-button-close')]")))
+#wd.find_elements(By.XPATH, "//*[contains(@class, 'privacy-policy-notice-with-close-button-close')]")[1].click()
+
+# Use JavaScript to remove the consent manager element from the page
+try:
+    driver.execute_script("document.getElementById('transcend-consent-manager').remove();")
+    print("Consent manager removed from the page.")
+except Exception as e:
+    print("Error while removing consent manager: ", e)
+
+# Find match-by-match
+match_by_match_tab = driver.find_element(By.ID, "ui-id-3")
+
+# Click on match-by-match
+match_by_match_tab.click()
+
+# Wait for the page to load
+time.sleep(3)
+
 
 
